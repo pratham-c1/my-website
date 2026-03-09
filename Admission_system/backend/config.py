8bc2cfcd3c6ac298b91ba2c8f6830fde7f01059a
pratham-c1 <prathamchaulagai1@gmail.com>
2026-03-09 15:14:10 +0545

    Delete Admission_system directory

--- Admission_system/backend/config.py
+++
@@ -1,7 +0,0 @@
-# Database configuration
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',  # Change to your MySQL username
-    'password': 'aaravaayan@321',  # Change to your MySQL password
-    'database': 'school_admission'
-}
