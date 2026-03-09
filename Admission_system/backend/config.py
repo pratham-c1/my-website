e87ec6988fbab213172d382fe744e4625c6582ff
pratham-c1 <prathamchaulagai1@gmail.com>
2026-03-09 15:00:49 +0545

    Add files via upload

---
+++ Admission_system/backend/config.py
@@ -0,0 +1,7 @@
+# Database configuration
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',  # Change to your MySQL username
+    'password': 'aaravaayan@321',  # Change to your MySQL password
+    'database': 'school_admission'
+}
